7838348395231ded5f00edfd2b7da6560b92f9e3
陆智勇 <lzy12611@ly.com>
2017-06-26 15:24:24 +0800

    connect更新

--- Connect/connect/sample.js
+++ Connect/connect/sample.js
@@ -1,29 +1,16 @@
 var connect=require('connect'),
-	time=require('./request-time');
-	console.log(time)
-	var server=connect.createServer();
-	server.use(connect.logger('dev'));
-	server.use(time({time:500}));
-	server.use(function(req,res,next){
-		if('/a'==req.url) {
-			res.writeHead(200);
-			res.end('Fast!');
-		} else{
-			next();
-		}
-	});
-	server.use(function(req,res,next){
-		if('/b'==req.url){
-			setTimeout(function(){
-				res.writeHead(200);
-				res.end('Slow!');
-			},1000)
-		} else {
-			next();
-		}
-	});
-	server.listen(3000);
-	server.use('/a',function(req,res,next){
+time=require('./request-time');
+var server=connect.createServer();
+server.use(connect.logger('dev'));
+server.use(time({time:500}));
+server.use('/a',function(req,res,next){
+	res.writeHead(200);
+	res.end('Fast!');
+});
+server.use('/b',function(req,res,next){
+	setTimeout(function(){
 		res.writeHead(200);
-		res.end('Fast!');
-	});
+		res.end('Slow!');
+	},1000)
+});
+server.listen(3000);
